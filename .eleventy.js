bc46fd36ce5d53215d7cd08bb2e75a1e51d6bf17
gongz <jogt@foxmail.com>
2026-05-03 10:17:05 +0800

    Fix WeChat verification file path

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(config) {
   config.addPassthroughCopy("static");
   config.addPassthroughCopy("favicon.svg");
   config.addPassthroughCopy("netlify");
+  config.addPassthroughCopy("e4fa9ed9ad7776dc34e6f0c969e91c8d.txt");
 
   return {
     dir: {
