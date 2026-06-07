ad8597c35289221813b88d7366f02245794fb953
MarsAyong <629446034@qq.com>
2026-06-07 10:11:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,7 @@ module.exports = {
   siteName: 'Ayong的摄影日记',
   siteTitle: '记录生活，定格美好',
   siteDescription: '用镜头捕捉每一个值得珍藏的瞬间',
-  footer: {
-    social: [
-      { label: '小红书', url: 'https://xhslink.com/m/8MA6NoHQesz', brand: 'xiaohongshu' },
-      { label: '抖音', url: 'https://v.douyin.com/owyNGXrRXrA/', brand: 'douyin' },
-      { label: '微信：GongzVirAug' },
-      { label: '邮箱：jogt@foxmail.com', url: 'mailto:jogt@foxmail.com' }
+  
     ]
   }
 };
